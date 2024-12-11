341c1db1faa98732589ca8980ca7d9603c53d0b1
MD. Jobayer Hasan <99082271+Jobayer-hasan-rifat@users.noreply.github.com>
2024-12-10 22:59:54 -0800

    Update web.php

--- REMS/routes/web.php
+++ REMS/routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
+Route::get('/login', [AuthController::class, 'showLoginPage'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::get('/dashboard', [DashboardController::class, 'showDashboard'])->name('dashboard')->middleware('auth');
 Route::view('/', 'welcome');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
